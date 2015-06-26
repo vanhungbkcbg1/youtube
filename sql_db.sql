2d70051c9d10cb87fde2e546bfa95c55dd28b6df
vanhung <vanhungbkcbg1@gmail.com>
2015-06-26 22:58:50 +0700

    add sql file

---
+++ sql_db.sql
@@ -0,0 +1,49 @@
+/*
+SQLyog Ultimate v8.71 
+MySQL - 5.6.21 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `youtube_db`;
+
+/*Table structure for table `channel` */
+
+DROP TABLE IF EXISTS `channel`;
+
+CREATE TABLE `channel` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `link_channel` tinytext,
+  `catagory` tinytext COMMENT 'kenh upload len dailymotion',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `youtube` */
+
+DROP TABLE IF EXISTS `youtube`;
+
+CREATE TABLE `youtube` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id ',
+  `youtube_id` tinytext COLLATE utf8_unicode_ci COMMENT 'youtube video',
+  `title` text COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
+  `catagory` tinytext COLLATE utf8_unicode_ci,
+  `type` smallint(6) DEFAULT NULL COMMENT 'type=1:youtubevide,type=0:link',
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'link video',
+  `tag` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
